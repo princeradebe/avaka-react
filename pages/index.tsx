2c0176684b839c6eba291ac5cd046b0e0a961cd6
Prince Radebe <princeradebe@gmail.com>
2023-01-21 15:59:38 +0200

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,83 +4,37 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="font-montserrat overflow-x-hidden ">
       <Head>
-        <title>Create Next App</title>
+        <title>AVAKA - Think Beyond Audit | Audit, Consulting, Advisory, and Tax Services</title>
+        <meta name="title" content="AVAKA - Think Beyond Audit | Audit, Consulting, Advisory, and Tax Services" />
+        <meta name="description" content="AVAKA offers services to meet your internal audit, advisory and consulting needs. Utilizing our team of experienced accounting professionals AVAKA can provide your business with a variety of management consulting and other internal audit and advisory services." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://www.avaka.co.za/" />
+        <meta property="og:title" content="AVAKA - Think Beyond Audit | Audit, Consulting, Advisory, and Tax Services" />
+        <meta property="og:description" content="AVAKA offers services to meet your internal audit, advisory and consulting needs. Utilizing our team of experienced accounting professionals AVAKA can provide your business with a variety of management consulting and other internal audit and advisory services." />
+        <meta property="og:image" content="https://avaka.co.za/assets/avaka-logo-metadata.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="http://www.avaka.co.za/" />
+        <meta property="twitter:title" content="AVAKA - Think Beyond Audit | Audit, Consulting, Advisory, and Tax Services" />
+        <meta property="twitter:description" content="AVAKA offers services to meet your internal audit, advisory and consulting needs. Utilizing our team of experienced accounting professionals AVAKA can provide your business with a variety of management consulting and other internal audit and advisory services." />
+        <meta property="twitter:image" content="https://avaka.co.za/assets/avaka-logo-metadata.png" />
+
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,700;0,800;1,300;1,400;1,500;1,700;1,800&display=swap"
+          rel="stylesheet"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
-
 export default Home
